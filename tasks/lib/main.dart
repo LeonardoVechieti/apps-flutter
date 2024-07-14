ff1786556e2e8ca52595a1dfdf084dd7021015c9
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-07-14 15:24:34 -0300

    feat: scaffold com list view

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -13,30 +13,84 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter Demo'),
+          backgroundColor: Colors.blue,
+          title: const Text('Tasks'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: ListView(
+          children: const [
+            Task('Teste de descrição maior que o normal' ),
+            Task('teste'),
+            Task('teste'),
+            Task('teste'),
+            Task('teste'),
+            Task('teste'),
+            Task('teste'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: const Icon(Icons.add),
+        )
+      ),
+    );
+  }
+}
+
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+              //width: 100,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                    child: Text(
+                      nome,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(
+                        Icons.arrow_forward
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
