a5e7cba82545ac442c1ca7f6550086c4aa9219ae
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-07-21 16:02:05 -0300

    update: add image

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Tasks'),
         ),
         body: ListView(
-          children: const [
+          children:[
             Task('Teste de descrição maior que o normal' ),
             Task('teste'),
             Task('teste'),
@@ -44,12 +44,20 @@ class MyApp extends StatelessWidget {
 }
 
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
+
   const Task(this.nome,{super.key});
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+  @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -58,37 +66,76 @@ class Task extends StatelessWidget {
             Container(
               color: Colors.blue,
               height: 140,
-              //width: 100,
             ),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                        child: Image.network(
+                          'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.nome,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+                        },
+                        child: const Icon(
+                            Icons.arrow_forward
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 200,
-                    child: Text(
-                      nome,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          color: Colors.grey,
+                          value: nivel/10,
+                          backgroundColor: Colors.white,
+                          valueColor: const AlwaysStoppedAnimation<Color>(Colors.grey),
+                        ),
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(
-                        Icons.arrow_forward
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        'Nível: $nivel',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             )
           ],
         ),
